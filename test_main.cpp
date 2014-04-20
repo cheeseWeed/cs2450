0c01246b2668225c24929fcfb10f85a812c234ef
Kody Holman <kodyman17@me.com>
2014-04-19 20:02:06 -0600

    Update test file db

--- test_main.cpp
+++ test_main.cpp
@@ -12,32 +12,40 @@
 using namespace std;
 
 int main(int argc, const char * argv[]) {
+    string output;
+    stringstream ss;
 
+    Library lib ("library_file_test.ldb");
+    Date::Instance().AdvanceDate(5);
+    try{
+        lib.checkout(1, 1);
+        assert("testCheckout", false);
+    }
+    catch (exception e){
+        assert("testCheckout", true);
+    }
+    try{
+        lib.checkout(1, 99);
+        assert("testCheckoutFail", false);
+    }
+    catch (exception e){
+        assert("testCheckoutFail", true);
+    }
+    lib.checkin(1);
+    lib.validItemId(99);
+    lib.listItems(ss, ItemSearchOverdue);
+    
+    ss.str(std::string()); // clear ss
+    lib.listItems(ss, ItemSearchAll);
+    output = ss.str();
+    assert("List All Items", output.find("Jurassic Park"));
+    
+    lib.checkin(1);
+    lib.listPatronItems(ss, 1);
+    lib.listAllPatrons(ss);
+    lib.validPatronId(1);
 
-		Library lib ("library_file.ldb");
-		Date::Instance().AdvanceDate();
-		try{
-			lib.checkout(1, 1);
-			assert("testCheckout", false);
-		}
-		catch (exception e){
-			assert("testCheckout", true);
-		}
-		try{
-			lib.checkout(1, 99);
-			assert("testCheckoutFail", false);
-		}
-		catch (exception e){
-			assert("testCheckoutFail", true);
-		}
-		lib.checkin(1);
-		lib.validItemId(99);
-		lib.listItems(cout, ItemSearchOverdue);
-		lib.listItems(cout, ItemSearchAll);
-		lib.checkin(1);
-		lib.listPatronItems(cout, 1);
-		lib.listAllPatrons(cout);
-		lib.validPatronId(1);
-
+    cout << endl;
+    
 	return 0;
 }
